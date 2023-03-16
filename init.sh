bb178821c305eb0c92dd5a4dee39763ff4c70729
pika <993416727@qq.com>
2023-03-16 23:16:07 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,13 +9,15 @@ redis=1
 
 #安装node
 
+apt update
 apt install wget -y;
 apt insatll libevent -y;
+apt install build-essential -y 
 
 
 if [ $node -eq 1 ];then
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-nvm use 16
+nvm install --lts
 fi
 
 if [ $openresty -eq 1 ];then
@@ -32,8 +34,8 @@ fi
 
 if [ $php -eq 1 ];then 
 	add-apt-repository -y ppa:ondrej/php
-	apt install -y php8.0 php8.0-cli php8.0-fpm php8.0-pdo php8.0-mysql php8.0-bcmath php8.0-xml \
-	php8.0-redis php8.0-mongodb php8.0-mbstring php8.0-gd php8.0-curl php8.0-zip php8.0-dev 
+	apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-pdo php8.2-mysql php8.2-bcmath php8.2-xml \
+	php8.2-redis php8.2-mongodb php8.2-mbstring php8.2-gd php8.2-curl php8.2-zip php8.0-dev 
 fi
 
 if [ $mysql -eq 1 ];then
