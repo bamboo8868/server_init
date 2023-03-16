48153de9ba957b4cffc298ff800749f404b1cf45
pika <993416727@qq.com>
2023-03-16 23:19:12 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -35,7 +35,7 @@ fi
 if [ $php -eq 1 ];then 
 	add-apt-repository -y ppa:ondrej/php
 	apt install -y php8.2 php8.2-cli php8.2-fpm php8.2-pdo php8.2-mysql php8.2-bcmath php8.2-xml \
-	php8.2-redis php8.2-mongodb php8.2-mbstring php8.2-gd php8.2-curl php8.2-zip php8.0-dev 
+	php8.2-redis php8.2-mongodb php8.2-mbstring php8.2-gd php8.2-curl php8.2-zip php8.2-dev 
 fi
 
 if [ $mysql -eq 1 ];then
